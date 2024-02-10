4cde4a0079a5fa0f0670113bb6d10b4092d5aefd
Densu341 <denyirawan341@gmail.com>
2024-02-11 00:14:36 +0700

    feat modified:users validation data, pasword hash, and roleUser

--- models/users.js
+++ models/users.js
@@ -1,7 +1,6 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model, Sequelize } = require("sequelize");
+const bcrypt = require("bcrypt");
 module.exports = (sequelize, DataTypes) => {
   class Users extends Model {
     /**
@@ -13,11 +12,52 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Users.init({
-    fullname: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Users',
-  });
+  Users.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      fullname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role_id: {
+        type: DataTypes.UUID,
+      },
+    },
+    {
+      hooks: {
+        beforeCreate: async (user) => {
+          if (user.password) {
+            const salt = bcrypt.genSaltSync(10);
+            user.password = bcrypt.hashSync(user.password, salt);
+          }
+          if (!user.role_id) {
+            const roleUser = await Sequelize.models.Role.findOne({
+              where: { nama_role: "user" },
+            });
+            user.role_id = roleUser.id;
+          }
+        },
+      },
+      sequelize,
+      modelName: "Users",
+    }
+  );
   return Users;
 };
