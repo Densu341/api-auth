833525d506237e03340a54668996bd803e01e632
Densu341 <denyirawan341@gmail.com>
2024-02-10 21:59:16 +0700

    feat modified:routes auth

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const port = process.env.APPLICATION_PORT || 3000;
 dotenv.config();
+const authRouter = require("./routes/AuthRouter");
 
 // middleware
 app.use(express.json());
@@ -12,6 +13,9 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("API Start up and Running!");
 });
+
+// routing
+app.use(`/api/v1/auth`, authRouter);
 
 app.listen(port, () => {
   console.log(`app listening on http://localhost:${port}`);
