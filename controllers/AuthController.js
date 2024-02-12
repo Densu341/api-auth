07918927bdee44fef4da7228015c10ff3ed28e3b
Densu341 <denyirawan341@gmail.com>
2024-02-12 22:30:49 +0700

    feat create:signToken and loginUser Function

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,10 @@
 const { Users } = require("../models");
+const jwt = require("jsonwebtoken");
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.registerUser = async (req, res) => {
   try {
@@ -15,8 +21,11 @@ exports.registerUser = async (req, res) => {
       password: req.body.password,
     });
 
+    const token = signToken(newUser.id);
+
     return res.status(201).json({
       message: "User created successfully",
+      token,
       data: newUser,
     });
   } catch (error) {
@@ -25,4 +34,40 @@ exports.registerUser = async (req, res) => {
       error: error.errors.map((err) => err.message),
     });
   }
+};
+
+exports.loginUser = async (req, res) => {
+  // 1) check fungsi validasi
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({
+      status: "Fail",
+      message: "Validation error",
+      error: "Please Input Email or Password",
+    });
+  }
+
+  // 2) check apakah email ada dan password sudah benar
+  const userData = await Users.findOne({
+    where: {
+      email: req.body.email,
+    },
+  });
+  if (
+    !userData ||
+    !(await userData.CorrectPassword(req.body.password, userData.password))
+  ) {
+    return res.status(400).json({
+      status: "Fail",
+      message: "Error Login",
+      error: "Invalid Email or Password",
+    });
+  }
+
+  // 3) return token
+  const token = signToken(userData.id);
+  return res.status(200).json({
+    status: "Success",
+    message: "Login Success",
+    token,
+  });
 };
