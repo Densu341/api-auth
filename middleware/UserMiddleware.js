0006a20e3ca78392b4c457501c2b9a360d186a81
Densu341 <denyirawan341@gmail.com>
2024-02-16 02:51:00 +0700

    feat middleware:permission for role base routing

--- middleware/UserMiddleware.js
+++ middleware/UserMiddleware.js
@@ -1,16 +1,17 @@
 const jwt = require("jsonwebtoken");
-const { Users } = require("../models");
+const { Users, Role } = require("../models");
 
 // middleware can be use on protect routes
 exports.authMiddleware = async (req, res, next) => {
   // get token
   let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
-  }
+  //   if (
+  //     req.headers.authorization &&
+  //     req.headers.authorization.startsWith("Bearer")
+  //   ) {
+  //     token = req.headers.authorization.split(" ")[1];
+  //   }
+  token = req.cookies.jwt;
 
   if (!token) {
     return res.status(401).json({
@@ -42,8 +43,33 @@ exports.authMiddleware = async (req, res, next) => {
     });
   }
 
-  console.log("currentUser", currentUser);
   req.user = currentUser;
 
   next();
 };
+
+// example :
+// const { authMiddleware } = require("../middleware/UserMiddleware");
+// router.post("/reserve", authMiddleware, ReservRoom);
+
+exports.permissionUser = (...roles) => {
+  return async (req, res, next) => {
+    const rolesData = await Role.findByPk(req.user.role_id);
+    const userRole = rolesData.nama_role;
+
+    if (!roles.includes(userRole)) {
+      return next(
+        res.status(403).json({
+          status: 403,
+          message: "Forbidden Access",
+        })
+      );
+    }
+
+    next();
+  };
+};
+
+// example :
+// const { permissionUser } = require("../middleware/UserMiddleware");
+// router.post("/transaction", authMiddleware, permissionUser("admin"), Transaction);
