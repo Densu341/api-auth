9a26a826b8bacfb75ec5370594467e59d8c3ef74
Densu341 <denyirawan341@gmail.com>
2024-02-16 02:54:32 +0700

    feat create:route & functionality logout and me

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/AuthController");
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getMe,
+} = require("../controllers/AuthController");
+const { authMiddleware } = require("../middleware/UserMiddleware");
 
 router.post("/register", registerUser);
 router.post("/", loginUser);
+router.post("/logout", authMiddleware, logoutUser);
+router.get("/me", authMiddleware, getMe);
 
 module.exports = router;
